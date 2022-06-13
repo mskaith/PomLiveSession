a1291f3d8faaa7238717770307d081906d6af8e8
techbrain <tbs.mohitk@gmail.com>
2022-06-13 14:26:37 +0530

    Added the test case for login

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,43 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
+	
+	@FindBy(id="email")
+	public WebElement usrname;
+	
+	@FindBy(id="pass")
+	public WebElement pass;
+	
+	@FindBy(id="u_0_d_if")
+	public WebElement login;
+	
+	public WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		
+		this.driver=driver;
+		
+	}
+	
+	public void doLogin(String username, String password) {
+		
+//		usrname.sendKeys(username);
+//		pass.sendKeys(password);
+//		login.click();
+		
+		driver.findElement(By.id("email")).sendKeys("Xusuhukj");
+		driver.findElement(By.id("pass")).sendKeys("xuhiusadhsiuad");
+		driver.findElement(By.id("u_0_d_if")).click();
+		
+	}
+	
+	public void valiateLinks() {
+		
+	}
 
 }
---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.HomePage;
+
+public class LoginTest {
+
+	
+	@Test
+	public void loginTest() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com/");
+		
+		HomePage home = new HomePage(driver);
+		home.doLogin("xyz@gmail.com", "jshusdgs");
+		
+	}
+	
+}
