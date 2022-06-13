5bdce708f0bddbf197627cfc2386a36a4802c828
techbrain <tbs.mohitk@gmail.com>
2022-06-13 14:59:56 +0530

    Some modification with the constructor

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	
@@ -13,26 +14,25 @@ public class HomePage {
 	@FindBy(id="pass")
 	public WebElement pass;
 	
-	@FindBy(id="u_0_d_if")
+	@FindBy(name="login")
 	public WebElement login;
 	
-	public WebDriver driver;
-	
-	public HomePage(WebDriver driver) {
-		
-		this.driver=driver;
-		
-	}
+	//When you want to use HomePage home = new HomePage(driver);
+//	public HomePage(WebDriver driver) {
+//		
+//		PageFactory.initElements(driver, this);
+//		
+//	}
 	
 	public void doLogin(String username, String password) {
 		
-//		usrname.sendKeys(username);
-//		pass.sendKeys(password);
-//		login.click();
+		usrname.sendKeys(username);
+		pass.sendKeys(password);
+		login.click();
 		
-		driver.findElement(By.id("email")).sendKeys("Xusuhukj");
-		driver.findElement(By.id("pass")).sendKeys("xuhiusadhsiuad");
-		driver.findElement(By.id("u_0_d_if")).click();
+//		driver.findElement(By.id("email")).sendKeys("Xusuhukj");
+//		driver.findElement(By.id("pass")).sendKeys("xuhiusadhsiuad");
+//		driver.findElement(By.id("u_0_d_if")).click();
 		
 	}
 	
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -21,7 +22,8 @@ public class LoginTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.facebook.com/");
 		
-		HomePage home = new HomePage(driver);
+		//HomePage home = new HomePage(driver);  // When you want to use the constructor
+		HomePage home = PageFactory.initElements(driver, HomePage.class);
 		home.doLogin("xyz@gmail.com", "jshusdgs");
 		
 	}
